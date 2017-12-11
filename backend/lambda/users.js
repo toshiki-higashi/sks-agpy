d0b6237199d279d70cd269517ea6adbefefb1ebc
hirase <33487596+tatsuya-hirase@users.noreply.github.com>
2017-12-11 22:26:08 +0900

    SAM Localのサンプルコード追加

---
+++ backend/lambda/users.js
@@ -0,0 +1,24 @@
+'use strict';
+
+exports.handler = (event, context, callback)=> {
+  let id = event.pathParameters.users || false;
+  switch(event.httpMethod){
+    case "GET":
+      if(id){
+        callback(null,{statusCode: 200,body:"user get function id"+id});
+        return;
+      };
+      callback(null,{statusCode: 200,body:"users function"});
+      break;  
+
+    case "POST":
+      callback(null,{statusCode: 200,body:"users function"});
+      break;  
+
+    case "DELETE":
+      callback(null,{statusCode: 200,body:"user function id"+id});
+      break;
+    default:
+      callback(null,{statusCode: 501});
+  };
+}
