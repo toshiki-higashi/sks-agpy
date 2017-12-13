c5df812f26c4a6dae54e252aa4fee5d9aefb2bca
[sks]tatsuya hirase <33487596+tatsuya-hirase@users.noreply.github.com>
2017-12-13 22:06:07 +0900

    Wrote definition for DynamoDBLocal

--- backend/lambda/items/index.js
+++ backend/lambda/items/index.js
@@ -1,26 +1,52 @@
 'use strict';
+var AWS = require('aws-sdk');
 
+// Delete on production environment
+AWS.config.update({
+  region: "ap-northeast-1",
+  endpoint: "http://localhost:8000"
+});
+
+
+var db = AWS.DynamoDB();
+
+db.scan(params,function(err,data){
+  if(err){
+    console.log("Error",err);
+  }else{
+    data.Items.forEach(function(element, index, array){
+      console.log(element);
+    });
+  }
+});
+  
 exports.handler = (event, context, callback)=> {
-  console.log(event);
+
   let id = event.pathParameters?event.pathParameters.item:false;
+
+
   switch(event.httpMethod){
     case "GET":
       if(id){
-        callback(null, { statusCode: 200, body: "GET with param" + id});
+        callback(null, { statusCode: 200, body: "GET with param " + id});
         return;
       };
       callback(null, { statusCode: 200, body: "GET without param" });
       break;  
 
     case "POST":
-      callback(null, { statusCode: 200, body: "POST" });
+      callback(null, { statusCode: 200, body: "POST with param " + id});
+      break;  
+
+    case "PUT":
+      callback(null, { statusCode: 200, body: "PUT with param " + id});
       break;  
 
     case "DELETE":
-      callback(null, { statusCode: 200, body: "DELETE" });
+      callback(null, { statusCode: 200, body: "DELETE with param " + id});
       break;
 
     default:
-      callback(null, { statusCode: 200, body: "default" });
+      callback(null, { statusCode: 501, body: "Not implemented" });
   };
 }
--- backend/lambda/users/index.js
+++ backend/lambda/users/index.js
@@ -1,28 +1,26 @@
 'use strict';
 
-exports.handler = (event, context, callback) => {
-  callback(null,{
-    statusCode: 200,
-    body: "Lorem ipsum"
-  });
-  // let id = event.pathParameters.users || false;
-  // switch(event.httpMethod){
-  //   case "GET":
-  //     if(id){
-  //       callback(null,{statusCode: 200,body:"user get function id"+id});
-  //       return;
-  //     };
-  //     callback(null,{statusCode: 200,body:"users function"});
-  //     break;  
+exports.handler = (event, context, callback)=> {
+  console.log(event);
+  let id = event.pathParameters?event.pathParameters.item:false;
+  switch(event.httpMethod){
+    case "GET":
+      if(id){
+        callback(null, { statusCode: 200, body: "GET with param" + id});
+        return;
+      };
+      callback(null, { statusCode: 200, body: "GET without param" });
+      break;  
 
-  //   case "POST":
-  //     callback(null,{statusCode: 200,body:"users function"});
-  //     break;  
+    case "POST":
+      callback(null, { statusCode: 200, body: "POST" });
+      break;  
 
-  //   case "DELETE":
-  //     callback(null,{statusCode: 200,body:"user function id"+id});
-  //     break;
-  //   default:
-  //     callback(null,{statusCode: 501});
-  // };
+    case "DELETE":
+      callback(null, { statusCode: 200, body: "DELETE" });
+      break;
+
+    default:
+      callback(null, { statusCode: 200, body: "default" });
+  };
 }
