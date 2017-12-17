e19d7e4e16c89eb906bb837ddfaa70d7e6fb06d1
[sks]tatsuya hirase <33487596+tatsuya-hirase@users.noreply.github.com>
2017-12-17 17:20:47 +0900

    implemented GET method with DynamoDB Local

--- backend/lambda/items/index.js
+++ backend/lambda/items/index.js
@@ -1,52 +1,105 @@
 'use strict';
 var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
 
-// Delete on production environment
 AWS.config.update({
+  // Does the region specified here needs to be consistent
+  // with the downloaded version of DynamoDB Local?
   region: "ap-northeast-1",
-  endpoint: "http://localhost:8000"
+
+  // Endpoint should be an alias for loopback interface
+  // Change to https://dynamodb.aws-region.amazonaws.com
+  // (replace aws-region with the region you use)
+  endpoint: "http://192.16.123.1:8000"
 });
+var db = new AWS.DynamoDB();
 
-
-var db = AWS.DynamoDB();
-
-db.scan(params,function(err,data){
-  if(err){
-    console.log("Error",err);
-  }else{
-    data.Items.forEach(function(element, index, array){
-      console.log(element);
-    });
-  }
-});
-  
 exports.handler = (event, context, callback)=> {
 
+  // common parameters to call DynamoDB API
+  var params = {
+    TableName: "agpy-items"
+  };
+
+  // Get path parameter, set false if not given
   let id = event.pathParameters?event.pathParameters.item:false;
 
+  // Set validation function and database operation
+  var responseBody = null;
+  var validator = null;
+  var operation = null;
 
   switch(event.httpMethod){
     case "GET":
+
       if(id){
-        callback(null, { statusCode: 200, body: "GET with param " + id});
-        return;
-      };
-      callback(null, { statusCode: 200, body: "GET without param" });
-      break;  
+
+        validator = function(){};
+
+        operation = function(){
+          // Get an item with the key given
+          params.Key = {"id":{"S":id}};
+          db.getItem(params, function(err,data){
+            if(err)console.log(err);
+            else console.log(data);
+          });
+        };
+
+      }else{
+
+        validator = function(){};
+
+        operation = function(){
+          // Get all items in agpy-item
+          db.scan(params, function(err,data){
+            if(err)console.log(err);
+            else console.log(data);
+          });
+          responseBody = 'list of items...'
+        };
+      }
+      break;
 
     case "POST":
-      callback(null, { statusCode: 200, body: "POST with param " + id});
+      validator = function(){};
+
+      // Create a new item in apgy-items
+      // What if a generated uuid collide with any of existing uuids?
+      operation = function(){
+        params.Item = {
+          "id":{"S":uuid.v4()},
+          "name":{"S":"foo"}};
+        db.putItem(params,function(err,data){
+          if(err)console.log(err);
+          else console.log(data);
+        });
+      };
       break;  
 
     case "PUT":
-      callback(null, { statusCode: 200, body: "PUT with param " + id});
+      // What attributes should be required? Only changed or all of them?
+      validator = function(){};
+
+      // Update an item with the given id
+      operation = function(){
+      };
       break;  
 
     case "DELETE":
-      callback(null, { statusCode: 200, body: "DELETE with param " + id});
+      validator = function(){};
+
+      operation = function(){
+        // Delete an item with the given id
+      };
       break;
 
     default:
-      callback(null, { statusCode: 501, body: "Not implemented" });
-  };
+      callback(null, {statusCode: 501, body: "Not implemented"});
+      return;
+  }; 
+
+  // Apply validation and execute CRUD
+  validator();
+  operation();
+
 }
