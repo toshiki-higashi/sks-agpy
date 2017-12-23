baece3801f916e3c17b11ce2a7dd2ffa0579c398
imada <26128342+yuki-imada@users.noreply.github.com>
2017-12-23 20:40:32 +0900

    データをAPIから取得する形に変更。ただし一覧の取得のみ。

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,6 +20,7 @@ div#wrapper.container-fluid
 </template>
 
 <script>
+import axios from 'axios';
 // 子コンポーネント呼び出し手順1
 import Header from './components/header.vue';
 import Menu from './components/menu.vue';
@@ -48,82 +49,21 @@ export default {
         borrowDate: '借りる日',
         returnDate: '返す日',
       },
-      // 表のデータ※本来はここをAPIで引っ張ってくる
-      itemLists: [
-        {
-          id: '100123',
-          category: '本',
-          name: '仕事に必要なことはすべて映画で学べる  映画監督 押井守',
-          location: '本社',
-          place:'8F本棚A-1',
-          getDate: '2017/10/01',
-          owner: 'A氏',
-          borrower: 'B氏',
-          borrowDate: '2017/10/31',
-          returnDate: '2017/11/10'
-        },
-        {
-          id: '100288',
-          category: '本',
-          name: 'Web API: The Good Parts  水野 貴明',
-          location: '本社',
-          place:'8F本棚C-8',
-          getDate: '2017/11/10',
-          owner: 'C氏',
-          borrower: 'D氏',
-          borrowDate: '2017/11/08',
-          returnDate: '2017/11/15'
-        },
-        {
-          id: '200288',
-          category: 'PC',
-          name: 'raspberry pi 3',
-          location: '本社',
-          place:'第1倉庫室',
-          getDate: '2017/11/10',
-          owner: '第1チーム',
-          borrower: 'E氏',
-          borrowDate: '2017/11/13',
-          returnDate: '2017/11/17'
-        },
-        {
-          id: '200456',
-          category: 'PC',
-          name: 'mac mini',
-          location: '大阪オフィス',
-          place:'第1倉庫室',
-          getDate: '2017/11/10',
-          owner: '第1チーム',
-          borrower: '',
-          borrowDate: '',
-          returnDate: ''
-        },
-        {
-          id: '300789',
-          category: 'その他',
-          name: '寝袋',
-          location: '大阪オフィス',
-          place:'第1倉庫室',
-          getDate: '2017/11/10',
-          owner: '第1チーム',
-          borrower: 'imada',
-          borrowDate: '2017/11/10',
-          returnDate: '2017/11/11'
-        },
-        {
-          id: '300288',
-          category: 'その他',
-          name: '掃除機',
-          location: '大阪オフィス',
-          place:'第1倉庫室',
-          getDate: '2017/11/10',
-          owner: '第1チーム',
-          borrower: '',
-          borrowDate: '',
-          returnDate: ''
-        }
-      ],
+      // 表のデータ(APIを叩いた結果をここに格納)
+      itemLists: []
     }
+  },
+  // 検証環境ではnpmパッケージjson-serverを叩いて、
+  // db.jsonファイルの中身を取り出している
+  // beforeMountが良いかは検討の余地あり
+  // 詳細はVue.js公式ドキュメントのライフサイクルフックを参照
+  beforeMount: function () {
+    let url = "http://localhost:3000/itemLists";
+    axios.get(url).then((response) => {
+      this.itemLists = response.data;
+    }).catch( error => {
+      console.log(error);
+    });
   }
 }
 </script>
